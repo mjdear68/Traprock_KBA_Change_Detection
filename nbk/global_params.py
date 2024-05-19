b0da2c62d2904d83899888b0c044d244c7113d4e
Michael Dear <mjdear68@gmail.com>
2024-05-19 18:06:02 +1000

    Added Traprock.geojson; updated global_params.py

--- nbk/global_params.py
+++ nbk/global_params.py
@@ -4,13 +4,13 @@ netcdf_dir = '../data/netcdf/'
 img_dir = '../data/img/'
 vector_dir = '../data/vector/'
 geotiff_dir = '../data/geoTIFF/'
-climate_dir = '../data/climate/BoM_Mudgee_Airport/monthly_temp_rain/'
+climate_dir = '../data/climate/BoM/'
 
-area_unit = 'ha'
-area_unit_val = 10**4
+area_unit = '900m^2'
+area_unit_val = 30**2
 
-spatial_res = 100 # Spatial resolution of the raster dataset in metres
+spatial_res = 30 # Spatial resolution of the raster dataset in metres
 crs = 3577
 
 # Study area abbreviation for exports etc
-study_area_abbrev = 'mudgee'
+study_area_abbrev = 'Traprock'
